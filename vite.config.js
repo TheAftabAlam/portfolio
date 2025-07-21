b5a633916c5e60e6f8bbf2d34f7643f5f2783e93
theaftabalam <aftabalamsi12@gmail.com>
2025-07-21 23:40:08 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // base: "https://github.com/TheAftabAlam/portfolio-website", 
   plugins: [react()],
+  base: '/portfolio-website/',
 })
 
