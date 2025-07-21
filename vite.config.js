ed1c1a0f43655ff44f2c2fe7a5938bc70da30fa9
theaftabalam <aftabalamsi12@gmail.com>
2025-07-21 23:42:07 +0530

    Final changes before deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  // base: "https://github.com/TheAftabAlam/portfolio-website", 
+  base: '/portfolio-website/', // 👈 This is the fix
   plugins: [react()],
-  base: '/portfolio-website/',
-})
-
+});
